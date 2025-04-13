7037a2d98196c7292b262ad6dca42039abb0605e
wangyijin <252120053@qq.com>
2025-04-13 16:06:18 +0800

    vue-3th

---
+++ 全栈-王艺瑾-vue第三次任务/src/components/GeneralCpn.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{ header }}</h1>
+    <div>
+        <slot name="firstline"></slot>
+    </div>
+    <!-- <div>
+        <slot name="secondline">这是第二个插槽的默认内容</slot>
+    </div> -->
+</template>
+<script>
+export default {
+    name: 'GeneralCpn',
+    props: {
+        header: {
+            type: String,
+            default: '这里是该组件的标题props'
+        }
+    }
+}
+</script>
